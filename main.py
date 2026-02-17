5e916bd5cd0caa413699b247714369f8fc289644
OMER IDRIS <omabrm2000@gmail.com>
2026-02-17 14:19:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-import time, os, random
+import time, os, random, pandas
 
 def clear():
   os.system('cls' if os.name == 'nt' else 'clear')
-clear()
 
-print('*** Student Management System ***\n\nDescription:\nThis project is about a student management system, you will be able to add, enroll and graduate students.')
-
+data_loc = 'D:/Advanced Py/library/lesson 01/students_data.csv'
 students = {}
 
+ # تحميل الملف على القاموس للتحقق مباشرة
+if os.path.exists(data_loc):
+   ds = pandas.read_csv(data_loc)
+   for _, row in ds.iterrows():
+      students[str(row['student_id'])] = {
+         'student_id': str(row['student_id']),
+         'name': row['name'],
+         'major': row['major'],
+         'GPA': float(row['GPA']),
+         'status': row['status'],
+                                          }
+      
+
+clear()
+print("""
+Student Management System\n
+This program manages student records using a CSV file as a database.
+It allows the user to:
+- Register new students
+- Enroll registered students
+- Graduate enrolled students
+- Search for students by ID, name, or major
+- Display all students\n
+All student data is stored persistently in a CSV file.    """)
+
+
+# --------- Displaying function
 def display_student(id):
   s = students[id]
   print(f'\nStudent ID: {id}  Name: {s['name']}  Major: {s['major']}  GPA: {s['GPA']}  Status: {s['status']}')
- 
+
+# --------- Registering function
 def register_student():
   clear()
   while True:
@@ -32,7 +58,7 @@ def register_student():
     major = input('Enter major: ').title()
     while not major: major = input('Major is required!\nEnter major: ').title()
 
-      # Getting GPA...
+    # Getting GPA...
     while True:
         try:
             GPA = float(input(f'What is {name} GPA? '))
@@ -49,47 +75,75 @@ def register_student():
       'major': major,
       'GPA': round(GPA,2),
       'status': 'registered'}
+    
+    with open(data_loc, 'a+') as file:
+       file.write(f'\n{student_id},{name},{major},{GPA},registered')
+
     print(f'Student "{name}" with major "{major}" registered to system with ID "{student_id}".')
     if input('Add another student? y/n ').lower() != 'y':
       break
 
+# --------- Enrolling function
 def enroll_student():
-  while True:
-    clear()
-    print('Enrolling a student.... \n')
-    student_id = input("Enter the Student ID to enroll: ")
-    if student_id in students:
-      if students[student_id]['status'] == 'registered':
-        print(f'Student {students[student_id]["name"]} enrolled successfully....')
-        students[student_id]['status'] = 'enrolled'
-      elif students[student_id]['status'] == 'enrolled':
-        print('Sorry! this student is already enrolled...')
-      elif students[student_id]['status'] == 'graduated':
-        print('Sorry! this student has already graduated...')
+    while True:
+        clear()
+        print('Enrolling a student.... \n')
+        student_id = input("Enter the Student ID to enroll: ").strip()
 
-    else:
-        print('Student not found in the system.')
-    if input('Enroll another student? y/n ').lower() != 'y':
-      break
+        if student_id in students:
+            sdd = students[student_id]
+            if sdd['status'] == 'registered':
 
+                sdd['status'] = 'enrolled'
+                print(f'Student {sdd["name"]} enrolled successfully....')
+
+                # reload dataframe fresh
+                df = pandas.read_csv(data_loc)
+
+                df.loc[df['student_id'] == student_id, 'status'] = 'enrolled'
+
+                df.to_csv(data_loc, index=False)
+
+            elif sdd['status'] == 'enrolled':
+                print('Sorry! this student is already enrolled...')
+            elif sdd['status'] == 'graduated':
+                print('Sorry! this student has already graduated...')
+        else:
+            print('Student not found in the system.')
+
+        if input('Enroll another student? y/n ').lower() != 'y':
+            break
+
+# --------- Graduating function        
 def graduate_student():
   while True:
     clear()
     print('Graduating a student.... \n')
     student_id = input('Enter Student ID to graduate: ')
-      # رحلة البحث عن عروس 🥳😂
+
     if student_id in students:
-      sdd = students[student_id] # to make things shorter
-      if sdd['status'] == 'graduated':
-        print("Sorry! this student has already graduated.")
-      elif sdd['status'] == 'enrolled':
+      sdd = students[student_id]
+      if sdd['status'] == 'enrolled':
+
         if input(f'Are you sure you want to graduate {sdd["name"]}? y/n: ') == 'y': # confirm serious actions
             print(f'Congratulations! 🌺 student {sdd["name"]} has graduated successfully')
             sdd['status'] = 'graduated'
+        
+            # reload dataframe fresh
+            df = pandas.read_csv(data_loc)
+
+            df.loc[df['student_id'] == student_id, 'status'] = 'graduated'
+
+            df.to_csv(data_loc, index=False)
+      
         else:
             continue
+      
+      elif sdd['status'] == 'graduated':
+        print("Sorry! this student has already graduated.")
       else:
         print("This student hasn't enrolled yet.")
+
     else:
       print('Student not found in the system.')
 
@@ -97,10 +151,12 @@ def graduate_student():
       break
 
 
+# --------- Searching function
 def search_student():
     while True:
         found = [] 
         Search_by = input('\nSearch by:\n1. ID\n2. Name\n3. Major\n\nEnter from 1-3: ')
+
         if Search_by == '1':
             Search_ID = input('Enter student ID to search please: ').strip()
             if Search_ID in students:
@@ -131,8 +187,8 @@ def search_student():
         if input('Search for another student? y/n ').lower() != 'y':
             break
 
+# ------ execution code
 while True:
-  clear()
   entered = input('''
 ==== Menu: ====\n
 1. Add Student
@@ -140,9 +196,8 @@ while True:
 3. Graduate Student
 4. Dispaly all Students
 5. Search for Student
-6. Edit Student Info
-7. Exit\n
-Enter your choice from 1-7: ''')
+6. Exit\n
+Enter your choice from 1-6: ''')
   
   if entered == '1':
     register_student()
@@ -162,10 +217,8 @@ Enter your choice from 1-7: ''')
   
   elif entered == '5':
      search_student()
-
-  # elif entered == '6':
-  #    edit_student()
   
-  elif entered == '7':
+  elif entered == '6':
     break
+  clear()
   
